2e158bfaf2e1fc1cfb2343371ccac4446dc11898
Omotola Babasola <omotolababasola@Omotolas-MacBook-Pro.local>
2025-12-22 19:26:48 +0000

    Add Route53 HostedZone for babalawo dev

--- lib/personal-website-cdk-stack.ts
+++ lib/personal-website-cdk-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
+import * as route53 from 'aws-cdk-lib/aws-route53';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PersonalWebsiteCdkStack extends cdk.Stack {
@@ -7,6 +8,23 @@ export class PersonalWebsiteCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // Route53 Hosted Zone for babalawo.dev
+    const hostedZone = new route53.HostedZone(this, 'BabalawoDevHostedZone', {
+      zoneName: 'babalawo.dev',
+      comment: 'Hosted zone for babalawo.dev domain'
+    });
+
+    // Output the hosted zone ID and name servers for domain configuration
+    new cdk.CfnOutput(this, 'HostedZoneId', {
+      value: hostedZone.hostedZoneId,
+      description: 'Route53 Hosted Zone ID for babalawo.dev'
+    });
+
+    new cdk.CfnOutput(this, 'NameServers', {
+      value: cdk.Fn.join(', ', hostedZone.hostedZoneNameServers || []),
+      description: 'Name servers for babalawo.dev domain'
+    });
 
     // example resource
     // const queue = new sqs.Queue(this, 'PersonalWebsiteCdkQueue', {
--- test/personal-website-cdk.test.ts
+++ test/personal-website-cdk.test.ts
@@ -1,17 +1,13 @@
-// import * as cdk from 'aws-cdk-lib/core';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as PersonalWebsiteCdk from '../lib/personal-website-cdk-stack';
+import * as cdk from 'aws-cdk-lib/core';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as PersonalWebsiteCdk from '../lib/personal-website-cdk-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/personal-website-cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new PersonalWebsiteCdk.PersonalWebsiteCdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+describe('PersonalWebsiteCdkStack', () => {
+  test('matches snapshot', () => {
+    const app = new cdk.App();
+    const stack = new PersonalWebsiteCdk.PersonalWebsiteCdkStack(app, 'MyTestStack');
+    const template = Template.fromStack(stack);
+    
+    expect(template.toJSON()).toMatchSnapshot();
+  });
 });
