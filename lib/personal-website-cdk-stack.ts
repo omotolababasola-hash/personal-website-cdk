11f56848be528014d4a6853ca236d7777c632866
Omotola Babasola <omotolababasola@Omotolas-MacBook-Pro.local>
2025-12-23 00:23:56 +0000

    Add S3 bucket behind Cloudfront

--- lib/personal-website-cdk-stack.ts
+++ lib/personal-website-cdk-stack.ts
@@ -1,6 +1,10 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class PersonalWebsiteCdkStack extends cdk.Stack {
@@ -12,7 +16,6 @@ export class PersonalWebsiteCdkStack extends cdk.Stack {
       comment: 'Hosted zone for babalawo.dev domain'
     });
 
-    // Output the hosted zone ID and name servers for domain configuration
     new cdk.CfnOutput(this, 'HostedZoneId', {
       value: hostedZone.hostedZoneId,
       description: 'Route53 Hosted Zone ID for babalawo.dev'
@@ -23,7 +26,57 @@ export class PersonalWebsiteCdkStack extends cdk.Stack {
       description: 'Name servers for babalawo.dev domain'
     });
 
-    // CodePipeline for CI/CD using CDK Pipelines
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      bucketName: `babalawo-dev-website-${this.account}`,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    const distribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
+      defaultBehavior: {
+        origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+      },
+      domainNames: ['babalawo.dev'],
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ],
+    });
+
+    new route53.ARecord(this, 'WebsiteAliasRecord', {
+      zone: hostedZone,
+      recordName: 'babalawo.dev',
+      target: route53.RecordTarget.fromAlias(new route53Targets.CloudFrontTarget(distribution)),
+    });
+
+    new cdk.CfnOutput(this, 'WebsiteBucketName', {
+      value: websiteBucket.bucketName,
+      description: 'S3 bucket name for website content'
+    });
+
+    new cdk.CfnOutput(this, 'DistributionId', {
+      value: distribution.distributionId,
+      description: 'CloudFront distribution ID'
+    });
+
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.distributionDomainName,
+      description: 'CloudFront distribution domain name'
+    });
+
+    new cdk.CfnOutput(this, 'WebsiteUrl', {
+      value: `https://babalawo.dev`,
+      description: 'Website URL'
+    });
+
     const pipeline = new CodePipeline(this, 'PersonalWebsitePipeline', {
       pipelineName: 'personal-website-pipeline',
       synth: new ShellStep('TestAndDeploy', {
