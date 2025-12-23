b4319e33faa541f963696eb146c185766bda6ebe
Omotola Babasola <omotolababasola@Omotolas-MacBook-Pro.local>
2025-12-23 00:53:24 +0000

    Move app resources into separate stack from the pipeline stack and add that stack as a separate stage

--- lib/personal-website-cdk-stack.ts
+++ lib/personal-website-cdk-stack.ts
@@ -1,93 +1,65 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-import * as route53 from 'aws-cdk-lib/aws-route53';
-import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { PersonalWebsiteAppStack } from './personal-website-app-stack';
 
 export class PersonalWebsiteCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const hostedZone = new route53.HostedZone(this, 'BabalawoDevHostedZone', {
-      zoneName: 'babalawo.dev',
-      comment: 'Hosted zone for babalawo.dev domain'
-    });
-
-    new cdk.CfnOutput(this, 'HostedZoneId', {
-      value: hostedZone.hostedZoneId,
-      description: 'Route53 Hosted Zone ID for babalawo.dev'
-    });
-
-    new cdk.CfnOutput(this, 'NameServers', {
-      value: cdk.Fn.join(', ', hostedZone.hostedZoneNameServers || []),
-      description: 'Name servers for babalawo.dev domain'
-    });
-
-    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
-      bucketName: `babalawo-dev-website-${this.account}`,
-      publicReadAccess: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    });
-
-    const distribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
-      defaultBehavior: {
-        origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
-      },
-      domainNames: ['babalawo.dev'],
-      defaultRootObject: 'index.html',
-      errorResponses: [
-        {
-          httpStatus: 404,
-          responseHttpStatus: 200,
-          responsePagePath: '/index.html',
-        },
-      ],
-    });
-
-    new route53.ARecord(this, 'WebsiteAliasRecord', {
-      zone: hostedZone,
-      recordName: 'babalawo.dev',
-      target: route53.RecordTarget.fromAlias(new route53Targets.CloudFrontTarget(distribution)),
-    });
-
-    new cdk.CfnOutput(this, 'WebsiteBucketName', {
-      value: websiteBucket.bucketName,
-      description: 'S3 bucket name for website content'
-    });
-
-    new cdk.CfnOutput(this, 'DistributionId', {
-      value: distribution.distributionId,
-      description: 'CloudFront distribution ID'
-    });
-
-    new cdk.CfnOutput(this, 'DistributionDomainName', {
-      value: distribution.distributionDomainName,
-      description: 'CloudFront distribution domain name'
-    });
-
-    new cdk.CfnOutput(this, 'WebsiteUrl', {
-      value: `https://babalawo.dev`,
-      description: 'Website URL'
-    });
-
     const pipeline = new CodePipeline(this, 'PersonalWebsitePipeline', {
       pipelineName: 'personal-website-pipeline',
-      synth: new ShellStep('TestAndDeploy', {
-        input: CodePipelineSource.gitHub('omotolababasola-hash/personal-website-cdk', 'main'),
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('omotolababasola-hash/personal-website-cdk', 'main', {
+          authentication: cdk.SecretValue.secretsManager('github-token')
+        }),
         commands: [
           'npm ci',
           'npm run test',
           'npm run build',
-          'npx cdk deploy'
+          'npx cdk synth'
         ]
-      })
+      }),
+      codeBuildDefaults: {
+        rolePolicy: [
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: [
+              'ssm:GetParameter',
+              'ssm:GetParameters'
+            ],
+            resources: [
+              `arn:aws:ssm:${this.region}:${this.account}:parameter/cdk-bootstrap/*`
+            ]
+          })
+        ]
+      }
     });
-   }
+
+    // Add deployment stage for the application stack
+    const deployStage = new PersonalWebsiteDeployStage(this, 'Deploy', {
+      env: {
+        account: this.account,
+        region: this.region
+      }
+    });
+
+    pipeline.addStage(deployStage);
+
+    // Output pipeline information
+    // new cdk.CfnOutput(this, 'PipelineName', {
+    //   value: pipeline.pipeline.pipelineName,
+    //   description: 'CodePipeline name for personal website'
+    // });
+  }
+}
+
+// Stage class for deployment
+class PersonalWebsiteDeployStage extends cdk.Stage {
+  constructor(scope: Construct, id: string, props?: cdk.StageProps) {
+    super(scope, id, props);
+
+    new PersonalWebsiteAppStack(this, 'PersonalWebsiteApp');
+  }
 }
